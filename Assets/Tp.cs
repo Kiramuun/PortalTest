d6639df2d427132c8374b03c8f372f7c5d1ef307
Kiramuun <deimonlight@live.fr>
2023-12-17 23:05:41 +0100

    Initial commit

---
+++ Assets/Tp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tp : MonoBehaviour
+{
+    public Transform _transformTp;
+    public GameObject _player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Teleport());
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(1);
+        _player.transform.position = new Vector3(_transformTp.transform.position.x, _transformTp.transform.position.y, _transformTp.transform.position.z);
+    }
+}
